8b5fdd5dcaeee8c83bb432fd3729ed4b902241b7
joeltio <joeltiojinhon@gmail.com>
2017-06-25 23:07:45 +0800

    Updated project info and entry point

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject proficiency "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "An environment for clojure to run"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot proficiency.core)
--- src/proficiency/core.clj
+++ src/proficiency/core.clj
@@ -1,6 +1,7 @@
-(ns proficiency.core)
+(ns proficiency.core
+  (:gen-class))
 
-(defn foo
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [& args]
+  (println "Hello, World!"))
